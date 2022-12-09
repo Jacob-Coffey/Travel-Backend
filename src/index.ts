c42c34507a07c912e97c9d53397cb8fa2d3cc19b
leeal <leealexandra58@gmail.com>
2022-12-08 19:39:17 -0500

    fixes

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as functions from "firebase-functions";
+import express from 'express';
+import cors from 'cors';
+import { yelpRoutes } from "./routes/YelpApi";
+import { databaseRoutes } from "./routes/DataBaseRoutes";
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.use("/", yelpRoutes);
+app.use("/db", databaseRoutes)
+
+export const api = functions.https.onRequest(app);
